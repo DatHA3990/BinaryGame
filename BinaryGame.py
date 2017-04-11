94ee5d27e2ffbb083df1719ad60e0760f2169150
DatHA3990 <dathayt@gmail.com>
2017-04-11 14:53:46 -0400

    soo many comments!!!

--- BinaryGame.py
+++ BinaryGame.py
@@ -1,44 +1,50 @@
 import random # to generate random number
 
-bit = 5
+bit = 5 # how many bit do you want the game to be
 
-print("Binary game : 10-bit max")
+print("Binary game : 5-bit max") # print simple instructions
 print()
 
+# generate a random number
 def getRandomNum():
-    return random.randrange(0,2**bit) # generate a random number
+    return random.randrange(0,2**bit) # return a randomly generated number
 
+# number to binary
 def getBinary(num):
-    return bin(num)[2:] # put that random number to binary
+    return bin(num)[2:] # return the binary number of the parameter
 
+# get user attempt
 def getUserNum():
-    userNumber = input()
+    userNumber = input() # get an input from the user
     try:
-        userNumber = int(userNumber)
-        return userNumber
+        userNumber = int(userNumber) # if it works,
+        return userNumber # return the number
     except:
-        getUserNum()
+        getUserNum() # otherwise, try till the answer is good
 
+# check if user attempt and random number are the same
 def check(x,y):
-    return x == y
+    return x == y # returna boolean : true is they are the same and false if not
 
+# main function
 def main():
-    randNum = getRandomNum()
-    randBin = getBinary(randNum)
+    randNum = getRandomNum() # get the random number
+    randBin = getBinary(randNum) # translate it to binary
 
-    print("Here is you new number : "),
-    print(randBin)
+    print("Here is you new number : "), # print short info
+    print(randBin) # print the binary
     #print(randomNumber) # just for easy debugging
 
-    userNum = getUserNum()
+    userNum = getUserNum() # get the users attempt
 
-    if check(randNum,userNum):
-        print("Correct.")
-    else :
-        print("Not correct. The answer was " + str(randNum))
+    if check(randNum,userNum): # check and if it is good
+        print("Correct.") # print correct
+    else : # otherwise
+        print("Not correct. The answer was " + str(randNum)) #print incorrect and print the number so that the user can learn
 
     print()
     print()
 
-while 1:
-    main()
+# infinite loop
+while 1: 
+    main() # call the main function
